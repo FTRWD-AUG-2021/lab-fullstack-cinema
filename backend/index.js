7cbc70803ef1c3c14d21871cf15339e9f3cb89a1
jbaker825 <jbaker825@gmail.com>
2021-09-27 10:36:06 -0500

    it. 2

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const app = express();
 const http = require("http");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const app = express();
-console.log("hi");
+app.use(cors())
+const MONGODB_URI = "mongodb://localhost:27017/Movie";
+const Movie = require("./models/Movie")
 
-app.listen(process.env.PORT || 5000);
+mongoose
+      .connect(MONGODB_URI, {
+        // useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(async (self) => {
+        console.log(`Connected to the database: "${self.connection.name}"`);
+      });
+
+app.get("/", (req, res) => {
+    res.send("Nothing Here");
+  });
+  
+  app.get("/api/movies", async (req, res) => {
+    const movies = await Movie.find({})
+    res.json(movies);
+  });
+
+
+app.listen(5000);
--- frontend/src/components/Movies.js
+++ frontend/src/components/Movies.js
@@ -1,7 +1,24 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Movies = () => {
-  return <div>MOVIES</div>;
-};
+  let [movies, setMovies] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/movies').then((res) => {
+      console.log(res.data)
+      setMovies(res.data);
+  })
+}, [])
+return (
+<div>
+  {movies.map((movie) => {
+    return (
+    <h1>{movie.title}</h1>
+    )
+  })}
+</div>
+)}
+
 
 export default Movies;
